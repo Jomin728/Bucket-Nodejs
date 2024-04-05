4e43835c6bd2d0eb1ec9620ef99f1195a0d88704
Jomin Rajesh K <Jomin.Rajesh.K1@ibm.com>
2024-04-05 11:31:35 +0530

    notification updated

--- notification-server/controllers/notification.js
+++ notification-server/controllers/notification.js
@@ -12,33 +12,38 @@ var transporter = nodemailer.createTransport({
 
 const handlebarOptions = {
     viewEngine: {
-        partialsDir: path.resolve('../views/'),
+        partialsDir: path.resolve('./views/'),
         defaultLayout: false,
     },
-    viewPath: path.resolve('../views/'),
+    viewPath: path.resolve('./views/'),
 };
 
 transporter.use('compile', hbs(handlebarOptions))
 
 
-exports.sendNotification = async (res,req,next) => {
+exports.sendNotification = async (req,res,next) => {
 
     const mailOptions = {
         from:'"Bucket" <bucket.com>',
+        template: "email",
         to: req.query.email,
         subject:'New File shared with you in Bucket',
         context:{
             name:req.query.email,
             filename:req.query.filename,
-            sender:req.query.user
+            sender:req.query.sender
         }
     }
 
     try {
         await transporter.sendMail(mailOptions);
+        res.send({message:'endpoint reached'})
         res.status(200).end()
       } catch (error) {
         console.log(`Nodemailer error sending email`, error);
+        res.send({message:'error while sending '})
+        res.status(400).end()
+
       }
 
 }
